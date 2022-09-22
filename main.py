5309af4869b95614f771a40212457a09a707ce1a
rafter1515 <96643634+rafter1515@users.noreply.github.com>
2022-09-22 03:59:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,59 @@
+
 import hmac
 import base64
 from hashlib import sha1
+from hmac import new
 import os
-
-
-def generate_device_Id():
-    identifier = os.urandom(20)
-    return ("32" + identifier.hex() + hmac.new(bytes.fromhex("76b4a156aaccade137b8b1e77b435a81971fbd3e"),
-                                               b"\x32" + identifier, sha1).hexdigest()).upper()
-
-
-import amino
+import secmail
+import aminofix as amino
 import json
 import threading
 import wget
+import time
 import requests
 import heroku3
+from bs4 import BeautifulSoup
 from new import emaill, passwordd, custompwd, chatlink, private, key, app_name, deviceid, nickname, replit
 
+
+PREFIX = bytes.fromhex("42")
+SIG_KEY = bytes.fromhex("F8E7A61AC3F725941E3AC7CAE2D688BE97F30B93")
+DEVICE_KEY = bytes.fromhex("02B258C63559D8804321C5D5065AF320358D366F")
+
+def generate_device_Id() -> str:
+    identifier = PREFIX + (os.urandom(20))
+    mac = new(DEVICE_KEY, identifier, sha1)
+    return f"{identifier.hex()}{mac.hexdigest()}".upper()
 
 def restart():
     heroku_conn = heroku3.from_key(key)
     botapp = heroku_conn.apps()[app_name]
     botapp.restart()
 
+def gen_email():
+    mail = secmail.SecMail()
+    email = mail.generate_email()
+    return email
+
+def get_message(email):
+    url="0"
+    try:
+        time.sleep(3)
+        f=email
+        mail = secmail.SecMail()
+        inbox = mail.get_messages(f)
+        print('done')
+        for Id in inbox.id:
+          msg = mail.read_message(email=f, id=Id).htmlBody
+          bs = BeautifulSoup(msg, 'html.parser')
+          images = bs.find_all('a')[0]
+          url = (images['href']+'\n')
+          if url is not None:
+            print('Vrification Url\n')
+            print(url)
+    except:
+        pass
+    return url
 
 client = amino.Client(deviceid)
 client.login(emaill, passwordd)
@@ -34,25 +64,22 @@ client.join_community(cid)
 sub = amino.SubClient(comId=cid, profile=client.profile)
 sub.join_chat(chatId)
 
-def code(url):
-    return requests.post('https://60da331a-4c43-4af6-931a-f738c5a1d607.id.repl.co/submundo', data={'image': url}).json()['captcha'][0]
-
 password = custompwd
 # client.devicee()
-de = generate_device_Id()
-client = amino.Client(de)
+device = generate_device_Id()
+client = amino.Client(device)
 for _ in range(3):
     try:
         os.remove("code.png")
     except:
         pass
-    dev = client.device_id
-    email = client.gen_email()
+    devicee = client.device_id
+    email = gen_email()
     print(email)
-    client.request_verify_code(email=email, dev=dev)
-    url = client.get_message(email)
+    client.request_verify_code(email=email, dev=devicee)
+    url = get_message(email)
     try:
-        code = requests.post('https://60da331a-4c43-4af6-931a-f738c5a1d607.id.repl.co/submundo', data={'image': url}).json()['captcha'][0]
+        code = requests.get(f"https://cynical-is-gay.herokuapp.com/captcha/solver/V1={url}").json()["captcha"]
     except:
         code = "001122"
     sub.send_message(chatId=chatId, message=("THE CODE is : "+str(code)), messageType=0)
@@ -62,7 +89,7 @@ for _ in range(3):
         d = {}
         d["email"] = str(email)
         d["password"] = str(password)
-        d["device"] = str(dev)
+        d["device"] = str(devicee)
         # t=json.dumps(d)
         print(d)
         requests.get(url=f"{replit}/api/save?email={str(email)}&password={str(password)}&device={str(dev)}")
@@ -71,20 +98,20 @@ for _ in range(3):
         pass
 
     # de=client.devicee()
-de = generate_device_Id()
-client = amino.Client(de)
+device = generate_device_Id()
+client = amino.Client(device)
 for _ in range(2):
     try:
         os.remove("code.png")
     except:
         pass
-    dev = client.device_id
-    email = client.gen_email()
+    devicee = client.device_id
+    email = gen_email()
     print(email)
-    client.request_verify_code(email=email, dev=dev)
-    url = client.get_message(email)
+    client.request_verify_code(email=email, dev=devicee)
+    url = get_message(email)
     try:
-        code = requests.post('https://60da331a-4c43-4af6-931a-f738c5a1d607.id.repl.co/submundo', data={'image': url}).json()['captcha'][0]
+        code = requests.get(f"https://cynical-is-gay.herokuapp.com/captcha/solver/V1={url}").json()["captcha"]
     except:
         code = "001122"
     sub.send_message(chatId=chatId, message=("THE CODE is : " + str(code)), messageType=0)
@@ -94,7 +121,7 @@ for _ in range(2):
         d = {}
         d["email"] = str(email)
         d["password"] = str(password)
-        d["device"] = str(dev)
+        d["device"] = str(devicee)
         # t=json.dumps(d)
         print(d)
         requests.get(url=f"{replit}/api/save?email={str(email)}&password={str(password)}&device={str(dev)}")
@@ -103,3 +130,4 @@ for _ in range(2):
         pass
 
 restart()
+
